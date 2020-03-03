c02dd78c1f309c411ec0aab02d0ab6b679757840
JvSe <joaovitosoares12@gmail.com>
2020-03-03 14:53:29 -0300

    Adicionando a pasta 'docs'

---
+++ docs/precache-manifest.932adb2ebd5716d4fc6b5f51d88b2453.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd63a7d3eb223ebdd930aa521fcdb295",
+    "url": "/studing-react-rellax/index.html"
+  },
+  {
+    "revision": "048bf7ab2b29dc6ad76b",
+    "url": "/studing-react-rellax/static/css/main.892b628c.chunk.css"
+  },
+  {
+    "revision": "941170a3c6e3447fc404",
+    "url": "/studing-react-rellax/static/js/2.3e6a4ce7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/studing-react-rellax/static/js/2.3e6a4ce7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "048bf7ab2b29dc6ad76b",
+    "url": "/studing-react-rellax/static/js/main.f6d9ba1c.chunk.js"
+  },
+  {
+    "revision": "336aaa0a2b4b413b6adb",
+    "url": "/studing-react-rellax/static/js/runtime-main.cfb60613.js"
+  }
+]);
